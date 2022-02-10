16aa376eb54e5968e7126426bdb9a93d5a51123e
userleaf <ulasyaprak@gmail.com>
2022-02-10 19:41:30 +0300

    test run

--- final/final_project/tulumba.py
+++ final/final_project/tulumba.py
@@ -12,33 +12,47 @@ class tulumba:
         for i,j,k in vals:
             self.colorVals[i] = j
         print(self.colorVals)
-        self.corners = np.zeros((7,7,4), dtype=int)
-        for i in range(7):
-            for j in range(7):
-                for k in range(4):
-                    if k == 0:
-                        self.corners[i,j,k] = (50+i*100,50+j*100)
-                    elif k == 1:
-                        self.corners[i,j,k] = (100+i*100,50+j*100)
-                    elif k == 2:
-                        self.corners[i,j,k] = (50+i*100,100+j*100)
-                    else:
-                        self.corners[i,j,k] = (100+i*100,100+j*100)
-
-        
-        self.arena = np.zeros(7,dtype=int)
 
     def run(self, img, info):
         # get time 
         startTime = time.time()
-        # get img and downsample
+        self.oppPos = np.zeros((len(info)-1,1),dtype=int)
+        # get img and downsample sort by val and store coordinates
+        self.scores = []
         for i in range(7):
             for j in range(7):
                 self.arena[i][j] = self.colorVals[img[75+i*50,75+j*50]]
+                
+                if self.arena[i][j] != 0:
+                    self.scores.append(self.arena[i][j],(i,j))
+        # sort by val descending
+        self.scores.sort(key=lambda x: x[0], reverse=True)
+
         # get info
         self.info = info
         for player in info['players']:
             if self.info[player] == self.name:
                 self.myPos = self.info[player][0]
-                self.myPoint = self.info[player][1]
+            else:
+                self.oppPos.append(self.info[player][0])
+        
+        for i in range(len(self.scores)):
+            for j in range(len(self.oppPos)):
+                if not self.manhattan(self.scores[i][1],self.oppPos[j]):
+                    self.scores[i][0] = 0
+        # sort by val descending
+        self.scores.sort(key=lambda x: x[0], reverse=True)
+        # make path
+        self.path = []
+        target = self.scores[0][1]
+        # return target to original scale
+        target = (target[0]*100+50,target[1]*100+50)
+        # find the closest corner
+        
+        
 
+    def manhattan(self, a, b):
+        if a[0]-b[0]+a[1]-b[1] < a[0]-self.myPos[0]+a[1]-self.myPos[1]:
+            return False
+        else:
+            return True
